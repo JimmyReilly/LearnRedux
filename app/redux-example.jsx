40df0721a2a3f94f84a96bd26a586253dfa0e423
reilly472 <reilly@whitewavemedia.com>
2017-05-25 10:14:20 -0400

    pure functions

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,3 +1,44 @@
-var redux = require('redux');
+/// PURE FUNCTION EXAMPLE
+// var redux = require('redux');
 
-console.log('Starting redux example');
+// console.log('Starting redux example');
+
+// // Pure function
+// function add (a, b) {
+//     return a + b;
+// }
+
+// // Not pure functions
+// var a = 3;
+// function add (a) {
+//     return a + b;
+// }
+
+// var result;
+// function add (a, b) {
+//     result = a + b;
+//     return result;
+// }
+
+// function add (a, b) {
+//     return a + b + new Date().getSeconds();
+// }
+
+// Made a pure funciton, doesn't change any outside values
+// function changeProp(obj) {
+//     return {
+//         ...obj,
+//         name: 'Jen'
+//     };
+//     // obj.name = 'Jen';
+//     // return obj;
+// }
+
+// var startingValue = {
+//     name: 'Jimmy',
+//     age: 25
+// }
+
+// var res = changeProp(startingValue);
+// console.log(startingValue);
+// console.log(res);
