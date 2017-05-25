5eb5ab055cc86bbab0e2febc20883cef90c645aa
reilly472 <reilly@whitewavemedia.com>
2017-05-25 16:18:47 -0400

    asunchronous actions

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,120 +1,170 @@
 var redux = require('redux');
+var axios = require('axios');
 
 console.log('Starting redux example');
 
 // Name reducer and action generators
-// ------------------------------------
+// -----------------
 var nameReducer = (state = 'Anonymous', action) => {
-    switch (action.type) {
-        case 'CHANGE_NAME':
-            return action.name;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return action.name;
+    default:
+      return state;
+  };
+};
 
 var changeName = (name) => {
-    return {
-        type: "CHANGE_NAME",
-        name
-    }
-}
+  return {
+    type: 'CHANGE_NAME',
+    name
+  }
+};
 
-// Hobby reducer and action generators
-// ------------------------------------
+// Hobbies reducer and action generators
+// -----------------
 var nextHobbyId = 1;
 var hobbiesReducer = (state = [], action) => {
-    switch (action.type) {
-        case 'ADD_HOBBY':
-            return [
-                ...state,
-                {
-                    id: nextHobbyId++,
-                    hobby: action.hobby
-                }
-            ];
-        case 'REMOVE_HOBBY':
-            return state.filter((hobby) => hobby.id !== action.id)
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case 'ADD_HOBBY':
+      return [
+        ...state,
+        {
+          id: nextHobbyId++,
+          hobby: action.hobby
+        }
+      ];
+    case 'REMOVE_HOBBY':
+      return state.filter((hobby) => hobby.id !== action.id);
+    default:
+      return state;
+  }
+};
 
 var addHobby = (hobby) => {
-    return {
-        type: 'ADD_HOBBY',
-        hobby
-    }
-}
+  return {
+    type: 'ADD_HOBBY',
+    hobby
+  };
+};
 
 var removeHobby = (id) => {
-    return {
-        type: 'REMOVE_HOBBY',
-        id
-    }
-}
+  return {
+    type: 'REMOVE_HOBBY',
+    id
+  };
+};
 
-var addMovie = (title, genre) => {
-    return {
-        type: 'ADD_MOVIE',
-        title,
-        genre
-    }
-}
-
-var removeMovie = (id) => {
-    return {
-        type: 'REMOVE_MOVIE',
-        id
-    }
-}
-
-// Movie reducer and action generators
-// ------------------------------------
+// Movies reducer and action generators
+// -----------------
 var nextMovieId = 1;
 var moviesReducer = (state = [], action) => {
-    switch (action.type) {
-        case 'ADD_MOVIE':
-            return [
-                ...state,
-                {
-                    id: nextMovieId++,
-                    title: action.title, 
-                    genre: action.genre
-                }
-            ]
-        case 'REMOVE_MOVIE':
-            return state.filter((movie) => movie.id !== action.id)
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case 'ADD_MOVIE':
+      return [
+        ...state,
+        {
+          id: nextMovieId++,
+          title: action.title,
+          genre: action.genre
+        }
+      ]
+    case 'REMOVE_MOVIE':
+      return state.filter((movie) => movie.id !== action.id)
+    default:
+      return state;
+  }
+};
+
+var addMovie = (title, genre) => {
+  return {
+    type: 'ADD_MOVIE',
+    title,
+    genre
+  };
+};
+
+var removeMovie = (id) => {
+  return {
+    type: 'REMOVE_MOVIE',
+    id
+  };
+};
+
+// Map reducer and action generators
+// -----------------
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+  switch (action.type) {
+    case 'START_LOCATION_FETCH':
+      return {
+        isFetching: true,
+        url: undefined
+      };
+    case 'COMPLETE_LOCATION_FETCH':
+      return {
+        isFetching: false,
+        url: action.url
+      };
+    default:
+      return state;
+  }
+};
+
+var startLocationFetch = () => {
+  return {
+    type: 'START_LOCATION_FETCH'
+  };
+};
+
+var completeLocationFetch = (url) => {
+  return {
+    type: 'COMPLETE_LOCATION_FETCH',
+    url
+  };
+};
+
+var fetchLocation = () => {
+  store.dispatch(startLocationFetch());
+
+  axios.get('http://ipinfo.io').then(function (res) {
+    var loc = res.data.loc;
+    var baseUrl = 'http://maps.google.com?q='
+
+    store.dispatch(completeLocationFetch(baseUrl + loc));
+  });
+};
 
 var reducer = redux.combineReducers({
-    name: nameReducer,
-    hobbies: hobbiesReducer,
-    movies: moviesReducer
+  name: nameReducer,
+  hobbies: hobbiesReducer,
+  movies: moviesReducer,
+  map: mapReducer
 });
 
 var store = redux.createStore(reducer, redux.compose(
-    window.devToolsExtension ? window.devToolsExtension() : f => f
+  window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
-//Subscribe to changes
+// Subscribe to changes
 var unsubscribe = store.subscribe(() => {
-    var state = store.getState();
+  var state = store.getState();
 
-    console.log('name is', state.name);
-    document.getElementById('app').innerHTML = state.name;
+  console.log('New state', store.getState());
 
-    console.log('New state', store.getState());
+  if (state.map.isFetching) {
+    document.getElementById('app').innerHTML = 'Loading...';
+  } else if (state.map.url) {
+    document.getElementById('app').innerHTML = '<a href="' + state.map.url + '" target="_blank">View Your Location</a>';
+  }
 });
-//Unsuscribe from changes
 // unsubscribe();
 
 var currentState = store.getState();
+console.log('currentState', currentState);
 
-store.dispatch(changeName('Jimmy'));
+fetchLocation();
+
+store.dispatch(changeName('Andrew'));
 
 store.dispatch(addHobby('Running'));
 store.dispatch(addHobby('Walking'));
@@ -123,48 +173,5 @@ store.dispatch(removeHobby(2));
 store.dispatch(changeName('Emily'));
 
 store.dispatch(addMovie('Mad Max', 'Action'));
-store.dispatch(addMovie('Star Wars', 'Action'));
+store.dispatch(addMovie('Start Wars', 'Action'));
 store.dispatch(removeMovie(1));
-
-
-/// PURE FUNCTION EXAMPLE
-
-// // Pure function
-// function add (a, b) {
-//     return a + b;
-// }
-
-// // Not pure functions
-// var a = 3;
-// function add (a) {
-//     return a + b;
-// }
-
-// var result;
-// function add (a, b) {
-//     result = a + b;
-//     return result;
-// }
-
-// function add (a, b) {
-//     return a + b + new Date().getSeconds();
-// }
-
-// Made a pure funciton, doesn't change any outside values
-// function changeProp(obj) {
-//     return {
-//         ...obj,
-//         name: 'Jen'
-//     };
-//     // obj.name = 'Jen';
-//     // return obj;
-// }
-
-// var startingValue = {
-//     name: 'Jimmy',
-//     age: 25
-// }
-
-// var res = changeProp(startingValue);
-// console.log(startingValue);
-// console.log(res);
